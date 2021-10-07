fd31b4412e5f8c32989d7437461928d2fd057707
shReya <shetshreya01@gmail.com>
2021-10-07 10:54:31 +0530

    first commit msg

---
+++ app.js
@@ -0,0 +1,16 @@
+
+  
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+ 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAu2O_pH8ukNlXkp1OiF6c16ozMr2-SNs8",
+    authDomain: "simple-insta-clone.firebaseapp.com",
+    projectId: "simple-insta-clone",
+    storageBucket: "simple-insta-clone.appspot.com",
+    messagingSenderId: "322237506995",
+    appId: "1:322237506995:web:0be038512df2a6b71117c6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
